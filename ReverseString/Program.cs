ac8689e0ac54181c7dbb3ac7870e11e9b324c939
hackercentral <levi.swain1@outlook.com>
2018-11-06 19:30:59 -0500

    initial commit

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Create a console app that takes a users input as a string and outputs the same string in reverse.
+
 namespace ReverseString
 {
     class Program
     {
+        static string reverseString(string input)
+        {
+            char[] letters = input.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a sentance");
+            Console.WriteLine("----------------------------------------");
+            string input = Console.ReadLine();
+            Console.WriteLine("The reverse of your sentance is: ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(reverseString(input));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Now can you enter a sentance backwards and have it print correctly? Try it out");
+            Console.WriteLine("----------------------------------------");
+            string backward = Console.ReadLine();
+            Console.WriteLine("The reverse of your sentance is: ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(reverseString(backward));
         }
     }
 }
